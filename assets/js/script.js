69baea356fe8eb2f4e1d9602ff5100d60421427d
Harry Liu <harryrrah0706@gmail.com>
2024-09-14 05:25:27 +0100

    code to send email

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,3 +157,15 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
   });
 }
+
+function loadEvents() {
+  var mailString;
+  function updateMailString() {
+      mailString = '?subject=' + encodeURIComponent($('#subject').val())
+          + '&body=' + encodeURIComponent($('#message').val());
+      $('#mail-link').attr('href',  'mailto:person@email.com' + mailString);
+  }
+  $( "#subject" ).focusout(function() { updateMailString(); });
+  $( "#message" ).focusout(function() { updateMailString(); });
+  updateMailString();
+}
